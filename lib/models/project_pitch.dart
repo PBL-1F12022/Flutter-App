7c49e74ce0d88abcc721a63b317da2988d59ccbf
RohanDoshi21 <rohan.doshi02@gmail.com>
2022-02-11 12:15:44 +0530

    Project Idea Model

---
+++ lib/models/project_pitch.dart
@@ -0,0 +1,17 @@
+class ProjectIdea {
+  late String id;
+  late String name;
+  late String description;
+  late int askingPrice;
+  late double equity;
+  late String owner;
+
+  ProjectIdea({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.askingPrice,
+    required this.equity,
+    required this.owner,
+  });
+}
